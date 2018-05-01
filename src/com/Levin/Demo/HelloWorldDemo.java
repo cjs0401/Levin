1b9e364970c5acaf2e1eb18b99466a4888f8aec1
cjs <cjs_91@163.com>
2018-05-01 14:38:55 +0800

    Initial commit

---
+++ src/com/Levin/Demo/HelloWorldDemo.java
@@ -0,0 +1,11 @@
+package com.Levin.Demo;
+
+/**
+ * Date:${Date}
+ * Author:Cjs
+ */
+public class HelloWorldDemo {
+    public static void main(String[] args) {
+        System.out.println("helloworld");
+    }
+}
